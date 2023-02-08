8f2e143f6d636cb1180414aad33360784aabe196
Sevag Avedissian <sevag.avedissian@gmail.com>
2023-02-07 22:35:15 -0800

    add api integration

--- utopian_eats/settings.py
+++ utopian_eats/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from config import databasePass
+from config import databasePass, secretKey
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-75^!4w0w3el$b&homr9+(89fe!4p=i&lfof1^=d=qoh4bkq-hw'
+SECRET_KEY = secretKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
